947a2f9ff1d2022d39edc334c574b81e5db0aefa
Denis Safiullin <fckninja@gmail.com>
2020-02-24 17:44:27 +0300

    Contract model new fields, user profile model

--- contract_management_app/contracts/models.py
+++ contract_management_app/contracts/models.py
@@ -4,10 +4,20 @@ from django.contrib.auth.models import User
 
 
 class Contract(models.Model):
+    contract_type_choices = [
+        ('service', 'Service'),
+        ('goods', 'Goods')
+    ]
     title = models.CharField(max_length=100)
     date_created = models.DateTimeField(default=timezone.now)
     # date_last_modified =
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    contract_type = models.CharField(max_length=7, choices=contract_type_choices, default='goods')  # service or goods
+    contract_file = models.FileField(upload_to='contracts')
+    contract_specification = models.FileField(upload_to='contracts', null=True, blank=True)
+    contract_appendix1 = models.FileField(upload_to='contracts', null=True, blank=True)
+    contract_appendix2 = models.FileField(upload_to='contracts', null=True, blank=True)
+    contract_file_signed = models.FileField(upload_to='contracts/signed', null=True, blank=True)
 
     def __str__(self):
         return self.title
--- contract_management_app/users/models.py
+++ contract_management_app/users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user_role_choices = [
+        ('PJM', 'Project Manager'),
+        ('CUS', 'Customer Interface'),
+        ('BUS', 'Business Case Accountable'),
+        ('LOG', 'Logistics Accountable'),
+        ('PRO', 'Production Accountable'),
+        ('QUA', 'Quality Accoutable'),
+        ('GEN', 'General Manager'),
+        ('PUR', 'Purchasing Accountable'),
+        ('ACC', 'Accounting')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_role = models.CharField(max_length=3, choices=user_role_choices, default='goods')
+    image = models.ImageField(default='default.jpg', blank=True, null=True, upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
